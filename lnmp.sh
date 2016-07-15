c08cb21633d6b4be89d7052a3892ffe301882257
ioioj5 <peng_du2007@qq.com>
2016-07-15 12:44:55 +0800

    lnmp安装脚本

---
+++ lnmp.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+# LNMP (linux + nginx + php + mysql) 安装脚本
+# @author ioioj5
+# @version 0.1
+# @date 2016/7/15
+
+# 1. 设置安装目录
+INSTALL_DIR=/data/server/
+INSTALL_LOG=/data/server/install.log
+
+# 2. 设置软件源
+
+# 3. 安装环境
+
+# 4. 安装nginx, php, mysql
+
+# 5. 优化系统
